206ff6caa944e6270d9a12b0f2334d427bbb8217
ilia makarov <ilia.makarov@me.com>
2023-04-06 09:15:57 +0300

    Wrote tests

---
+++ src/components/Home/__tests__/Home.test.tsx
@@ -0,0 +1,26 @@
+import { Home } from "../Home";
+import { getMeetingService } from "../../../services/zoom";
+import { render } from "../../../../testing";
+import mockMeetings from "../../../../testing/mocks/mockMeetings.json";
+import mockMeeting84350791442 from "../../../../testing/mocks/mockMeeting84350791442.json";
+import mockMeeting87605567807 from "../../../../testing/mocks/mockMeeting87605567807.json";
+import type { Meeting } from "../../../services/zoom/types";
+
+jest.mock("../../../services/zoom/getMeetingService");
+
+describe("Home", () => {
+  test("render", async () => {
+    (getMeetingService as jest.Mock)
+      .mockResolvedValueOnce(mockMeeting84350791442)
+      .mockResolvedValueOnce(mockMeeting87605567807);
+
+    const { findByText } = render(
+      <Home meetings={mockMeetings.meetings as Meeting[]} />,
+      { wrappers: { theme: true, query: true } },
+    );
+
+    expect(await findByText(/Retrospective Meeting/i)).toBeInTheDocument();
+    expect(await findByText(/All-Hands Engineering Stand Up/i)).toBeInTheDocument();
+    expect(await findByText(/Apps Stand Up/i)).toBeInTheDocument();
+  });
+});
