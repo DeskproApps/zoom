140992308817dd844d5dde477ba7f63a09b122e4
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-02-10 13:20:28 +0000

    [Feature] SC-180573 Update OAuth2 implementation to use latest app-sdk features

--- src/services/deskpro/setAccessTokenService.ts
+++ src/services/deskpro/setAccessTokenService.ts
@@ -1,6 +1,5 @@
 import { ACCESS_TOKEN_PATH } from "@/constants";
 import type { IDeskproClient } from "@deskpro/app-sdk";
-import type { OAuthToken } from "@/services/zoom/types";
 
 const setAccessTokenService = (client: IDeskproClient, access_token: string) => {
   return client.setUserState(ACCESS_TOKEN_PATH, access_token, { backend: true });
--- src/services/deskpro/setRefreshTokenService.ts
+++ src/services/deskpro/setRefreshTokenService.ts
@@ -1,6 +1,5 @@
 import { REFRESH_TOKEN_PATH } from "@/constants";
 import type { IDeskproClient } from "@deskpro/app-sdk";
-import type { OAuthToken } from "@/services/zoom/types";
 
 const setRefreshTokenService = (client: IDeskproClient, refresh_token: string) => {
   return client.setUserState(REFRESH_TOKEN_PATH, refresh_token, { backend: true });
