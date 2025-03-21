3c530233ce32065ceed9454987208641e7574237
OJ Abba <phavorabba@gmail.com>
2025-03-21 13:41:32 +0000

    [Chore] Kill unnecessary test case

--- src/pages/LoginPage/__tests__/LoginPage.test.tsx
+++ src/pages/LoginPage/__tests__/LoginPage.test.tsx
@@ -20,7 +20,7 @@ describe("LoginPage", () => {
       onSignIn: jest.fn(),
       isLoading: false,
     }));
-    const { findByRole } = render(<LoginPage/>, { wrappers: { theme: true }});
+    const { findByRole } = render(<LoginPage />, { wrappers: { theme: true } });
     const loginButton = await findByRole("link", { name: /Log In/i });
 
     await waitFor(() => {
@@ -37,7 +37,7 @@ describe("LoginPage", () => {
       authLink: "https://call-back.url/?code=123",
       onSignIn,
     }));
-    const { findByRole } = render(<LoginPage/>, { wrappers: { theme: true }});
+    const { findByRole } = render(<LoginPage />, { wrappers: { theme: true } });
 
     const loginButton = await findByRole("link", { name: /Log In/i });
 
@@ -46,21 +46,5 @@ describe("LoginPage", () => {
     });
 
     expect(onSignIn).toHaveBeenCalledTimes(1);
-  });
-
-  test("should navigate to /home after auth", async () => {
-    const navigate = jest.fn();
-    (useLogin as jest.Mock).mockImplementation(() => ({
-      error: null,
-      isAuth: true,
-      isLoading: false,
-      authLink: "https://call-back.url/?code=123",
-      onSignIn: jest.fn(),
-    }));
-    (useNavigate as jest.Mock).mockImplementation(() => navigate);
-    render(<LoginPage/>, { wrappers: { theme: true }});
-
-    expect(navigate).toHaveBeenCalledTimes(1);
-    expect(navigate).toHaveBeenCalledWith("/home");
   });
 });
